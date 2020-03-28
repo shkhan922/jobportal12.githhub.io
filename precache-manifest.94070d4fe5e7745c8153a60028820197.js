19fab5302fc217d823c261420f981e290281d406
shkhan922 <shkhan922@gmail.com>
2020-03-28 23:56:34 +0530

    Updates

---
+++ precache-manifest.94070d4fe5e7745c8153a60028820197.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38791e986542c14b3ab5c6bfda61343d",
+    "url": "/jobportal12.githhub.io/index.html"
+  },
+  {
+    "revision": "00e37a77055b4d2354ef",
+    "url": "/jobportal12.githhub.io/static/css/main.ff208674.chunk.css"
+  },
+  {
+    "revision": "ee04b3fc8b7ea5ff1243",
+    "url": "/jobportal12.githhub.io/static/js/2.13c09ee5.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/jobportal12.githhub.io/static/js/2.13c09ee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00e37a77055b4d2354ef",
+    "url": "/jobportal12.githhub.io/static/js/main.17db4cbc.chunk.js"
+  },
+  {
+    "revision": "7c7e539bf1494f610d7e",
+    "url": "/jobportal12.githhub.io/static/js/runtime-main.3afa37de.js"
+  }
+]);
